acc92a01cbefd6ee772dc16fe36c73c79e55950b
Anna Romashina <anny.200@proton.me>
2026-06-11 00:36:54 +0300

    Remove unnecessary stuff from PYI file

--- jlog/logger.pyi
+++ jlog/logger.pyi
@@ -1,4 +1,4 @@
-from typing   import Callable, Literal, Optional
+from typing   import Callable, Optional
 from io       import TextIOBase
 
 from .auxiliary import Filler
@@ -16,27 +16,6 @@ class JLog:
             offset_size:   int = 2,
             offset_filler: Callable[[int, int], str] = Filler.whitespace,
             line_term:     str = '\r\n'
-        ) -> None: ...
-
-    def _pop_colors(self, string: str) -> str: ...
-    def _gradientify(
-            self,
-            string: str,
-            fore_gradient: Optional[Gradient] = None,
-            back_gradient: Optional[Gradient] = None
-        ) -> str: ...
-    def _do_subs(
-            self,
-            string: str,
-            colors: bool = True
-        ) -> str: ...
-    def _cast(
-            self,
-            string:   str,
-            nl:       bool = True,
-            do_subs:  Literal['no', 'tty', 'everywhere'] = 'tty',
-            tty_only: bool = False,
-            skip_tty: bool = False
         ) -> None: ...
 
     def offset(self, amount: int = 1) -> None: ...
